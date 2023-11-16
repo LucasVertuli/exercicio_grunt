38516ed2d3b4cfbaa69c10f4d6473580af799721
LucasVertuli <136344537+LucasVertuli@users.noreply.github.com>
2023-11-15 22:17:06 -0300

    commit inicial

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+const { loadNpmTasks } = require("grunt");
+
+module.exports = function (grunt) {
+    
+    grunt.initConfig ({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+
+            production: {
+                options: {
+                    compress: true, 
+                },
+                files: {
+                    'main.min.css' : 'main.less'
+                }
+            }
+        },
+
+        uglify: {
+            target: {
+                files: {
+                    'main.min.js': 'main.js'
+                }
+            }
+        },
+
+        concurrent: {
+            target: ['less', 'uglify']
+        }
+
+    
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt,loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['concurrent'])
+}
